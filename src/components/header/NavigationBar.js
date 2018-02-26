dab8bf76a41329b9001b10b465aaa802d0ac33ed
alexLipnik1 <alex.lipnik111@gmail.com>
2018-02-26 13:56:01 +0200

    temp

--- src/components/header/NavigationBar.js
+++ src/components/header/NavigationBar.js
@@ -2,12 +2,15 @@ import { StyleSheet, Text, View, TouchableOpacity, Button, BackAndroid } from 'r
 import React from 'react';
 import styles from '../../styles/headerStyles/NavigationBar.style';
 import { ButtonGroup } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+const myIcon = (<Icon name="bars" size={25} />)
+
 
 export default class NavBar extends React.Component{
     constructor () {
         super()
         this.state = {
-            selectedIndex: 2
+            selectedIndex: 1
         }
         this.updateIndex = this.updateIndex.bind(this)
     }
@@ -17,29 +20,28 @@ export default class NavBar extends React.Component{
     }
 
     render(){
-        const buttons = ['Home', 'Tasks']
+        const buttons = [  myIcon , 'Tasks']
         const _buttons = ['', '']
         const { selectedIndex } = this.state
 
         return(
             <View style={styles.container}>
                 <View style={styles.nav}>
-
                     <ButtonGroup
                         onPress={this.updateIndex}
                         selectedIndex={selectedIndex}
                         buttons={buttons}
-                        textStyle={{fontSize: 16, fontWeight: "bold"}}
-                        selectedTextStyle={{fontSize: 20, fontWeight: "bold"}}
-                        containerStyle={{height: 60 ,width: 180, borderWidth: 0, marginTop: 0, marginBottom: 0, marginLeft: 0, marginRight: 0}}
-                        selectedButtonStyle= {{backgroundColor: '#cce6ff'}}
+                        textStyle={{ color: 'white', fontSize: 16, fontWeight: "bold"}}
+                        selectedTextStyle={{fontSize: 20, fontWeight: "bold", color: 'white'}}
+                        containerStyle={{backgroundColor: '#b3d9ff', height: 60 ,width: 150, borderWidth: 0, marginTop: 0, marginBottom: 0, marginLeft: 0, marginRight: 0}}
+                        // selectedButtonStyle= {{backgroundColor: '#cce6ff'}}
                         buttonStyle={{backgroundColor: '#b3d9ff', marginLeft: -1, marginRight: -1 }}
                     />
                     <ButtonGroup
                         onPress={this.updateIndex}
                         selectedIndex={selectedIndex}
                         buttons={_buttons}
-                        containerStyle={{height: 2 ,width: 180, borderWidth: 0, marginTop: 0, marginBottom: 0, marginLeft: 0, marginRight: 0}}
+                        containerStyle={{height: 2 ,width: 150, borderWidth: 0, marginTop: 0, marginBottom: 0, marginLeft: 0, marginRight: 0}}
                         selectedButtonStyle= {{backgroundColor: '#f2ffe6'}}
                         buttonStyle={{backgroundColor: '#b3d9ff', marginLeft: -1, marginRight: -1 }}
                     />
